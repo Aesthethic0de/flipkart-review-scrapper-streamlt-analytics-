941d84645a6e6f68f02ad2b4e1f3ba9ef10f7997
Aesthethic0de <mrsingh.someet@gmail.com>
2023-03-21 20:33:10 +0530

    building a flipkart api for getting all the comments and analyze in future.

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import logging
 
+flipkart_base = "https://www.flipkart.com"
 flipkart_url = "https://www.flipkart.com/search?q=" + "iphone"
 uClient = urlopen(flipkart_url)
 flipart_page = uClient.read()
-print(flipart_page)
+flipcart_html = bs(flipart_page,'html.parser')
+# print(flipcart_html)
+bigbox = flipcart_html.findAll("div" , {"class":"_2kHMtA"})
+del bigbox[0:3]
+
+product_links = []
+for i in range(len(bigbox)):
+    l = flipkart_base + bigbox[i].a['href']
+    product_links.append(l)
+
+print(product_links)
+
 
